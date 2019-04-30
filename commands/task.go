967f3475158fc5a08adc1b5c3514e11e15483d87
jiaguannan <jiaguannan@zhujia360.com>
2019-04-30 15:14:40 +0800

    task

--- commands/task.go
+++ commands/task.go
@@ -6,12 +6,13 @@
 package commands
 
 import (
-	"log"
-	"github.com/astaxie/beego/toolbox"
-	"fmt"
-	"github.com/gorilla/websocket"
 	"apitp/models"
+	"fmt"
+	"github.com/astaxie/beego/toolbox"
+	"github.com/gorilla/websocket"
+	"log"
 )
+
 var (
 	//客户端socket map
 	WsClients = make(map[*websocket.Conn]bool)
@@ -22,26 +23,28 @@ var (
 //创建webSocket定时任务
 func NewWebSocketTask() {
 	//定时不断的广播发送到页面上
-	//spec: 秒钟：0-59、分钟：0-59、小时：1-23、日期：1-31、月份：1-12、星期：0-6（0 表示周日）
-	tk := toolbox.NewTask("wsTask", "0/10 * * * * *", func() error {
-		select {
-		case msg := <-Broadcast:
-			fmt.Println("客户端数量：", len(WsClients))
-			for client := range WsClients {
-				err := client.WriteJSON(msg)
-				if err != nil {
-					log.Printf("发送消息出错 error: %v", err)
-					client.Close()
-					delete(WsClients, client)
+	go func() {
+		//spec: 秒钟：0-59、分钟：0-59、小时：1-23、日期：1-31、月份：1-12、星期：0-6（0 表示周日）
+		tk := toolbox.NewTask("wsTask", "0/10 * * * * *", func() error {
+			select {
+			case msg := <-Broadcast:
+				fmt.Println("客户端数量：", len(WsClients))
+				for client := range WsClients {
+					err := client.WriteJSON(msg)
+					if err != nil {
+						log.Printf("发送消息出错 error: %v", err)
+						client.Close()
+						delete(WsClients, client)
+					}
 				}
+			default:
+				fmt.Println("no data")
 			}
-		default:
-			fmt.Println("no data")
-		}
-		return nil
-	})
-	//tk.Run()
-	toolbox.AddTask("wsTask", tk)
-	//启动定时任务
-	toolbox.StartTask()
+			return nil
+		})
+		//tk.Run()
+		toolbox.AddTask("wsTask", tk)
+		//启动定时任务
+		toolbox.StartTask()
+	}()
 }
