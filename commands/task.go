f8acecf184ca38c3746857369a5785a1d5ea57a0
jiaguannan <15968881053@163.com>
2019-05-05 18:25:21 +0800

    并发问题

--- commands/task.go
+++ commands/task.go
@@ -11,6 +11,7 @@ import (
 	"github.com/astaxie/beego/toolbox"
 	"github.com/gorilla/websocket"
 	"log"
+	"sync"
 )
 
 var (
@@ -20,6 +21,8 @@ var (
 	WsClients = make(map[*websocket.Conn]bool)
 	//缓冲2000条记录
 	Broadcast = make(chan models.Message, 2000)
+	//数据交换管道
+	localWsChan = make(chan *websocket.Conn, 2000)
 )
 
 //创建webSocket定时任务
@@ -28,6 +31,7 @@ func NewWebSocketTask() {
 	go func() {
 		//spec: 秒钟：0-59、分钟：0-59、小时：1-23、日期：1-31、月份：1-12、星期：0-6（0 表示周日）
 		tk := toolbox.NewTask("wsTask", "0/2 * * * * *", func() error {
+			//从管道中读出客户端写入map中
 			func() {
 				select {
 				case ws := <-WsChan:
@@ -37,18 +41,40 @@ func NewWebSocketTask() {
 				}
 			}()
 			select {
+			//读取管道中消息
 			case msg := <-Broadcast:
+				//主线程等待
+				var wg sync.WaitGroup
 				fmt.Println("客户端数量：", len(WsClients))
+				//开启协程将客户端写入交换管道
 				for client := range WsClients {
-					err := client.WriteJSON(msg)
-					if err != nil {
-						log.Printf("发送消息出错 error: %v", err)
-						client.Close()
-						delete(WsClients, client)
-					}
+					localWsChan<- client
 				}
+				//开启多个协程并发向客户端推送消息
+				for i := 0; i < 4; i++ {
+					wg.Add(1)
+					go func(msg models.Message) {
+						for {
+							select {
+							case  client := <-localWsChan:
+								err := client.WriteJSON(msg)
+								if err != nil {//出错后移除客户端对象
+									log.Printf("发送消息出错 error: %v", err)
+									client.Close()
+									delete(WsClients, client)
+								}
+							default:
+								wg.Done()
+								return
+							}
+
+						}
+					}(msg)
+				}
+				wg.Wait()
 			default:
 				fmt.Println("no data")
+				return nil
 			}
 			return nil
 		})
